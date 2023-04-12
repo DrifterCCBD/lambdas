dd886e9b24b6767cc31a5a2cc704d982a8afe674
Ben Allison <bja2142@columbia.edu>
2023-04-11 20:44:32 -0400

    first commit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+find . -name "*.py" | while read fn_path; do
+	(
+	fn="$(printf "$fn_path" | sed 's+.*/++' | sed 's/\.py//')"
+	cp $fn_path lambda_function.py;
+	zip "${fn}.zip" lambda_function.py;
+	aws lambda update-function-code \
+		--function-name "${fn}" \
+		--zip-file "fileb://${fn}.zip";
+    rm "${fn}.zip"
+    )
+done
+rm lambda_function.py
