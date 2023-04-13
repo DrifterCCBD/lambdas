e5b34378f5b21a15d30eae9e3ffc466a7eff0a97
Helena Jonsdottir <helenajonsdottir@Helenas-MacBook-Pro-3.local>
2023-04-13 18:27:09 -0400

    get trips function for my trips base
    need to connect to db

--- trip/drifter-get-trip.py
+++ trip/drifter-get-trip.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 def lambda_handler(event, context):
     try:
@@ -6,14 +7,33 @@ def lambda_handler(event, context):
         # TODO: query database for all trips with this username
         # TODO: divide them by future and past trips
         # TODO: send the json object with two values, future and past trips
+        
+        
+        #TODO FIX:
+        # query_result = #query database
+        
+        past_trips = []
+        future_trips = []
+        
+        for data in query_result:
+            if datetime.datetime.strptime(data['start-date'], "%d/%m/%Y").date() < datetime.now():
+                past_trips.add(data)
+            else:
+                future_trips.add(data)
+        
+        return_json = {
+            future_trips: future_trips,
+            past_trips: past_trips
+        }
+
         return {
             'statusCode': 200,
-            'body': json.dumps('Hello from Lambda!'),
+            'body': json.dumps({'results': return_json}),
             'event' : event
         }
 
     except:
-        # TODO do other parameters check? like for available trips for rider
+        # TODO do other parameters check?
         print('username is not a parameter')
 
     # TODO implement
