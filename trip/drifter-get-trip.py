a5dbe8dab643b10e399029696b98cd361ce39e0b
Helena Jonsdottir <helenajonsdottir@Helenas-MacBook-Pro-3.local>
2023-04-13 17:15:15 -0400

    changed get trip

--- trip/drifter-get-trip.py
+++ trip/drifter-get-trip.py
@@ -1,6 +1,21 @@
 import json
 
 def lambda_handler(event, context):
+    try:
+        username = event['params']['querystring']['username']
+        # TODO: query database for all trips with this username
+        # TODO: divide them by future and past trips
+        # TODO: send the json object with two values, future and past trips
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!'),
+            'event' : event
+        }
+
+    except:
+        # TODO do other parameters check?
+        print('username is not a parameter')
+
     # TODO implement
     return {
         'statusCode': 200,
