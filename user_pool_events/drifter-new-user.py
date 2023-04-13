5836996f614a1d4e40818b2e34b909e8366793ca
Ben Allison <bja2142@columbia.edu>
2023-04-13 15:34:04 -0400

    fix drifter-new-user error

--- user_pool_events/drifter-new-user.py
+++ user_pool_events/drifter-new-user.py
@@ -3,7 +3,5 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!'),
-        'event' : event
+
     }
