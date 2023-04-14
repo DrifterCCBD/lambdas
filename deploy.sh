1339744503d1f7a714c4149b3c7016b63bb25b9c
Ben Allison <bja2142@columbia.edu>
2023-04-14 00:37:01 +0000

    move postgres to layer

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,26 @@ POSTGRES_HOSTNAME="database-1.cgwwmxrgggyq.us-east-1.rds.amazonaws.com"
 POSTGRES_PORT="5432"
 POSTGRES_DB="postgres"
 
-find . -name "*.py" | while read fn_path; do
-	fn="$(printf "$fn_path" | sed 's+.*/++' | sed 's/\.py//')"
-	aws lambda update-function-configuration \
-			--function-name "${fn}" \
-			--environment "{\"Variables\":{\"POSTGRES_HOSTNAME\":\"${POSTGRES_HOSTNAME}\",\"POSTGRES_PORT\":\"${POSTGRES_PORT}\",\"POSTGRES_DB\":\"${POSTGRES_DB}\",\"POSTGRES_USER\":\"${POSTGRES_USER}\",\"POSTGRES_PASS\":\"${POSTGRES_PASS}\"}}"
-	done
 
 test -e package && rm -r package
 mkdir -p package
 pip3 install --target package psycopg2-binary >/dev/null 2>/dev/null
 wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem -O package/global-bundle.pem
+( 
+	cd package
+	zip -r ../package.zip . >/dev/null 2>/dev/null
+)
+aws lambda publish-layer-version --layer-name postgres-lib \
+    --description "Postgres Library" \
+    --license-info "MIT" \
+    --zip-file fileb://package.zip \
+    --compatible-runtimes python3.8 python3.9 \
+    --compatible-architectures "x86_64"
+rm -r package package.zip 
+
 find . -path ./package -prune -o -name "*.py" | while read fn_path; do
 	fn="$(printf "$fn_path" | sed 's+.*/++' | sed 's/\.py//')"
 	cp $fn_path lambda_function.py;
-	( 
-		cd package
-		zip -r ../${fn}.zip . >/dev/null 2>/dev/null
-	)
 	zip "${fn}.zip" lambda_function.py;
 	aws lambda update-function-code \
 	--function-name "${fn}" \
@@ -29,5 +31,11 @@ find . -path ./package -prune -o -name "*.py" | while read fn_path; do
 	rm "${fn}.zip"
 done
 rm lambda_function.py
-rm -r package
 
+find . -path ./package -prune -o -name "*.py" | while read fn_path; do
+	fn="$(printf "$fn_path" | sed 's+.*/++' | sed 's/\.py//')"
+	aws lambda update-function-configuration \
+			--function-name "${fn}" \
+			--layers "postgres-lib" \
+			--environment "{\"Variables\":{\"POSTGRES_HOSTNAME\":\"${POSTGRES_HOSTNAME}\",\"POSTGRES_PORT\":\"${POSTGRES_PORT}\",\"POSTGRES_DB\":\"${POSTGRES_DB}\",\"POSTGRES_USER\":\"${POSTGRES_USER}\",\"POSTGRES_PASS\":\"${POSTGRES_PASS}\"}}"
+done
