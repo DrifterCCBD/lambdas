909473897e79ef099c6424c5ab6ad6b5f2310d4e
Helena Jonsdottir <helenajonsdottir@Helenas-MacBook-Pro-3.local>
2023-04-16 17:43:54 -0400

    available trips and my trips for rider ready

--- trip/drifter-get-trip.py
+++ trip/drifter-get-trip.py
@@ -6,32 +6,80 @@ import os
 
 
 def lambda_handler(event, context):
+    # We're sending parameters with the request from the frontend
+    # The parameters differ based on what is needed from the frontend
+    # If username is present, we're being asked for trips beloning
+    # either to a driver or a rider. The rider parameter gives us which.
+    # If available is present then we're being asked for all available
+    # trips for riders to select.
+    
     username = ''
+    available = ''
+    rider = ''
+    
+    db_host = os.environ.get("POSTGRES_HOSTNAME")
+    db_port = os.environ.get("POSTGRES_PORT")
+    db_name = os.environ.get("POSTGRES_DB")
+    db_user = os.environ.get("POSTGRES_USER")
+    db_pass = os.environ.get("POSTGRES_PASS")
+    postgres_connect_string = "host='{}' port='{}' sslmode=verify-full sslrootcert=/opt/global-bundle.pem dbname='{}' user='{}' password='{}'".format(
+        db_host,
+        db_port,
+        db_name,
+        db_user,
+        db_pass
+        )
 
     try:
         username = event['params']['querystring']['username']
+        rider = event['params']['querystring']['rider']
     except:
         print('username not present')
     
-    if username != '':
-
-        db_host = os.environ.get("POSTGRES_HOSTNAME")
-        db_port = os.environ.get("POSTGRES_PORT")
-        db_name = os.environ.get("POSTGRES_DB")
-        db_user = os.environ.get("POSTGRES_USER")
-        db_pass = os.environ.get("POSTGRES_PASS")
-        postgres_connect_string = "host='{}' port='{}' sslmode=verify-full sslrootcert=/opt/global-bundle.pem dbname='{}' user='{}' password='{}'".format(
-            db_host,
-            db_port,
-            db_name,
-            db_user,
-            db_pass
-            )
+    try:
+        available = event['params']['querystring']['available']
+    except:
+        print('available not present')
     
+    if username != '':
+        if rider == 'true':
+            query = "SELECT mt.trip_id, mt.origin, mt.destination, u.username AS driver_username, mt.start_date, mt.start_time\
+                FROM my_trip mt\
+                INNER JOIN driver d ON mt.driver_id = d.driver_id\
+                INNER JOIN users u ON d.user_id = u.user_id\
+                INNER JOIN rider_trip rt ON mt.trip_id = rt.trip_id\
+                INNER JOIN riders r ON rt.rider_id = r.rider_id\
+                INNER JOIN users ru ON r.user_id = ru.user_id\
+                WHERE ru.username = '" + username + "'"
+                
+            with psycopg.connect(postgres_connect_string) as db:
+                with db.cursor() as cur:
+                    cur.execute(query)
+                    query_result = cur.fetchall()
+    
+            print(query_result)
+            
+            past_trips = []
+            future_trips = []
+            
+            
+            for data in query_result:
+                return_data = {}
+                return_data['trip_id'] = data[0]
+                return_data['origin'] = data[1]
+                return_data['destination'] = data[2]
+                return_data['driver_username'] = data[3]
+                    
+                return_data['date_time'] = str(datetime.datetime.combine(data[4], data[5]))
+                
+                
+                if datetime.datetime.combine(data[4], data[5]) < datetime.datetime.now():
+                    past_trips.append(return_data)
+                else:
+                    future_trips.append(return_data)
 
-        with psycopg.connect(postgres_connect_string) as db:
-            with db.cursor() as cur:
-                query = "SELECT my_trip.trip_id, my_trip.driver_id, my_trip.origin, my_trip.destination, "\
+        else:
+            query = "SELECT my_trip.trip_id, my_trip.driver_id, my_trip.origin, my_trip.destination, "\
                 "my_trip.start_date, my_trip.start_time, string_agg(users.username, ', ') AS rider_firstnames "\
                 "FROM my_trip "\
                 "LEFT JOIN rider_trip ON my_trip.trip_id = rider_trip.trip_id "\
@@ -43,9 +91,58 @@ def lambda_handler(event, context):
                 "WHERE username = '" + username + "' "\
                 ") "\
                 "GROUP BY my_trip.trip_id"
+
+            with psycopg.connect(postgres_connect_string) as db:
+                with db.cursor() as cur:
+                    cur.execute(query)
+                    query_result = cur.fetchall()
+    
+            print(query_result)
+            past_trips = []
+            future_trips = []
+            
+            
+            for data in query_result:
+                return_data = {}
+                return_data['trip_id'] = data[0]
+                return_data['origin'] = data[2]
+                return_data['destination'] = data[3]
+                return_data['rider_firstnames'] = data[6]
+                    
+                return_data['date_time'] = str(datetime.datetime.combine(data[4], data[5]))
+                
+                
+                if datetime.datetime.combine(data[4], data[5]) < datetime.datetime.now():
+                    past_trips.append(return_data)
+                else:
+                    future_trips.append(return_data)
+            
+        return_json = {
+            'future_trips': future_trips,
+            'past_trips': past_trips
+        }
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'results': return_json}),
+            'event' : event
+        }
+    
+    elif available != '':
+        # Returns all trips that are in the future and don't have full capacity
+        
+        with psycopg.connect(postgres_connect_string) as db:
+            with db.cursor() as cur:
+                query = "SELECT t.trip_id, u.username AS driver_username, t.origin,\
+                t.destination, t.start_date, t.start_time\
+                FROM my_trip t\
+                INNER JOIN driver d ON t.driver_id = d.driver_id\
+                INNER JOIN users u ON d.user_id = u.user_id\
+                WHERE t.curr_capacity < t.max_capacity"
                 cur.execute(query)
                 query_result = cur.fetchall()
 
+        print(query_result)
         
         past_trips = []
         future_trips = []
@@ -53,22 +150,18 @@ def lambda_handler(event, context):
         
         for data in query_result:
             return_data = {}
-            return_data['trip_id'] = data[0]
+            return_data['date_time'] = str(datetime.datetime.combine(data[4], data[5]))
             return_data['origin'] = data[2]
             return_data['destination'] = data[3]
-            return_data['rider_firstnames'] = data[6]
-            return_data['date_time'] = str(datetime.datetime.combine(data[4], data[5]))
+            return_data['driver_username'] = data[1]
             
-            
-
             if datetime.datetime.combine(data[4], data[5]) < datetime.datetime.now():
                 past_trips.append(return_data)
             else:
                 future_trips.append(return_data)
         
         return_json = {
-            'future_trips': future_trips,
-            'past_trips': past_trips
+            'future_trips': future_trips
         }
 
         return {
