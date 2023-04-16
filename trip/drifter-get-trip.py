34a493b7ef618ddd1348586da91ca0cc3f8e31b8
Helena Jonsdottir <helenajonsdottir@Helenas-MacBook-Pro-3.local>
2023-04-16 16:29:08 -0400

    mytrips working

--- trip/drifter-get-trip.py
+++ trip/drifter-get-trip.py
@@ -14,9 +14,6 @@ def lambda_handler(event, context):
         print('username not present')
     
     if username != '':
-        # TODO: query database for all trips with this username
-        # TODO: divide them by future and past trips
-        # TODO: send the json object with two values, future and past trips
 
         db_host = os.environ.get("POSTGRES_HOSTNAME")
         db_port = os.environ.get("POSTGRES_PORT")
@@ -34,22 +31,21 @@ def lambda_handler(event, context):
 
         with psycopg.connect(postgres_connect_string) as db:
             with db.cursor() as cur:
-                query = "SELECT my_trip.trip_id, my_trip.driver_id, my_trip.origin, my_trip.destination,\
-                    my_trip.start_date, my_trip.start_time, GROUP_CONCAT(users.first_name SEPARATOR ', ') AS rider_firstnames\
-                        FROM my_trip\
-                        LEFT JOIN rider_trip ON my_trip.trip_id = rider_trip.trip_id\
-                        LEFT JOIN riders ON rider_trip.rider_id = riders.rider_id\
-                        LEFT JOIN users ON riders.user_id = users.user_id\
-                        WHERE my_trip.driver_id = (\
-                        SELECT driver_id\
-                        FROM users\
-                        WHERE username = '" + username + "'\
-                        )\
-                        GROUP BY my_trip.trip_id"
+                query = "SELECT my_trip.trip_id, my_trip.driver_id, my_trip.origin, my_trip.destination, "\
+                "my_trip.start_date, my_trip.start_time, string_agg(users.first_name, ', ') AS rider_firstnames "\
+                "FROM my_trip "\
+                "LEFT JOIN rider_trip ON my_trip.trip_id = rider_trip.trip_id "\
+                "LEFT JOIN riders ON rider_trip.rider_id = riders.rider_id "\
+                "LEFT JOIN users ON riders.user_id = users.user_id "\
+                "WHERE my_trip.driver_id = ( "\
+                "SELECT driver_id "\
+                "FROM users "\
+                "WHERE username = '" + username + "' "\
+                ") "\
+                "GROUP BY my_trip.trip_id"
                 cur.execute(query)
                 query_result = cur.fetchall()
 
-        print(query_result)
         
         past_trips = []
         future_trips = []
@@ -58,15 +54,14 @@ def lambda_handler(event, context):
         for data in query_result:
             return_data = {}
             return_data['trip_id'] = data[0]
-            return_data['driver_id'] = data[1]
             return_data['origin'] = data[2]
             return_data['destination'] = data[3]
-            return_data['date_time'] = data[4] + ' ' + data[5]
             return_data['rider_firstnames'] = data[6]
+            return_data['date_time'] = str(datetime.datetime.combine(data[4], data[5]))
+            
+            
 
-            input_datetime = datetime.strptime(return_data['date_time'], '%Y-%m-%d %H:%M:%S')
-
-            if input_datetime < datetime.now():
+            if datetime.datetime.combine(data[4], data[5]) < datetime.datetime.now():
                 past_trips.append(return_data)
             else:
                 future_trips.append(return_data)
