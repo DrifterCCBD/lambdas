01a54130661e215174048bdc6584f23550773fbc
Ben Allison <bja2142@columbia.edu>
2023-05-02 22:03:12 -0400

    notify user when background-check completes

--- sqs_events/drifter-process-background-check.py
+++ sqs_events/drifter-process-background-check.py
@@ -1,7 +1,11 @@
 import json
 import os
+import boto3
 import psycopg
 from psycopg.rows import dict_row
+
+lambda_client = boto3.client('lambda')
+
 
 
 def mark_completed(username):
@@ -17,13 +21,24 @@ def mark_completed(username):
         db_user,
         db_pass
         )
+    email = False
     with psycopg.connect(postgres_connect_string, row_factory=dict_row) as db:
         with db.cursor() as cur:
             query = "UPDATE driver SET background_check_complete = true WHERE user_id = (SELECT user_id FROM users WHERE username = %s)"
             query_values = (username,)
             cur.execute(query, query_values)
+            cur.execute("SELECT email from users where username = %s", query_values)
+            for row in cur:
+                email = row["email"]
         db.commit()
 
+    if email:
+        msg = {"msg": "Your background check has been completed and approved!", "dst" : email}
+        lambda_client.invoke(
+            FunctionName="drifter-process-notification",
+            InvocationType='Event',
+            Payload=json.dumps(msg)
+        )
 def lambda_handler(event, context):
     username = event.get("username",False)
     if username:
